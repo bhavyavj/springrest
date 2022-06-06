7737cdfdeff3aaa65c9f86da2b17540a1140ca8e
Bhavya Vijay <bvijay0560@gmail.com>
2022-06-06 16:06:48 +0530

    "Third commit"

--- src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
+++ src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
@@ -1,6 +1,8 @@
 package com.springrest.springrest.services;
 
+import com.springrest.springrest.dao.CourseDao;
 import com.springrest.springrest.entities.Course;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,48 +11,43 @@ import java.util.stream.Collectors;
 
 @Service
 public class CourseServiceImpl implements CourseService{
-    List<Course> list;
-    public CourseServiceImpl(){
-        list=new ArrayList<>();
-        list.add(new Course(111,"Java","this is java"));
-        list.add(new Course(222,"SpringBoot","this is SB"));
-    }
+//    List<Course> list;
+    @Autowired
+    private CourseDao courseDao;
+//    public CourseServiceImpl(){
+//        list=new ArrayList<>();
+//        list.add(new Course(111,"Java","this is java"));
+//        list.add(new Course(222,"SpringBoot","this is SB"));
+//    }
 
 
 
     @Override
     public List<Course> getCourses() {
-        return list;
+        return courseDao.findAll();
     }
 
     @Override
     public Course getCourse(long courseId) {
-        Course c=null;
-        for(Course course:list){
-            if(course.getId()==courseId){
-                c=course;
-                break;
-            }
-        }
-        return c;
+
+        return courseDao.getOne(courseId);
     }
     @Override
     public Course addCourse(Course course) {
-        list.add(course);
+       courseDao.save(course);
         return course;
     }
 
     @Override
     public Course updateCourse(Course course){
-        list.forEach(e->{if(e.getId()==course.getId()){
-        e.setTitle(course.getTitle());
-        e.setDescription(course.getDescription());
-        }
-        });
+
+        courseDao.save(course);
         return course;
     }
     @Override
     public void deleteCourse(long parseLong){
-        list=this.list.stream().filter(e->e.getId()!=parseLong).collect(Collectors.toList());
+        Course c=courseDao.getReferenceById(parseLong);
+        courseDao.delete(c);
+
     }
 }
