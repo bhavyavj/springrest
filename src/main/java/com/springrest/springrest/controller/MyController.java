ae9f44071f27c3a4c96d8c18e94d6c95b6336ac6
Bhavya Vijay <bvijay0560@gmail.com>
2022-06-06 15:07:37 +0530

    "Second commit"

--- src/main/java/com/springrest/springrest/controller/MyController.java
+++ src/main/java/com/springrest/springrest/controller/MyController.java
@@ -4,6 +4,8 @@ package com.springrest.springrest.controller;
 import com.springrest.springrest.entities.Course;
 import com.springrest.springrest.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,20 +20,40 @@ public class MyController {
         return "this  is home page";
     }
 
+    //get all courses
     @GetMapping("/courses")
     public List<Course> getCourses(){
 
         return this.courseService.getCourses();
     }
+    //get 1 course
     @GetMapping("/courses/{courseId}")
     public Course getCourse(@PathVariable String courseId){
 
         return this.courseService.getCourse(Long.parseLong(courseId));
     }
+    //add course
     @PostMapping("/courses")
     public Course getCourse(@RequestBody Course course){
 
         return this.courseService.addCourse(course);
+    }
+    //update course
+    @PutMapping("/courses")
+    public Course updateCourse(@RequestBody Course course){
+
+        return this.courseService.updateCourse(course);
+    }
+    @DeleteMapping("/courses/{courseId}")
+    public ResponseEntity<HttpStatus> deleteCourse(@PathVariable String courseId){
+        try {
+            this.courseService.deleteCourse(Long.parseLong(courseId));
+            return new ResponseEntity<>(HttpStatus.OK);
+
+        }
+        catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
